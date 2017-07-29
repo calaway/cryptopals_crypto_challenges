e52094c41a2e5ce09279169333fcca5651e5df0e
Calaway <calaway@users.noreply.github.com>
2017-07-29 14:54:18 -0600

    Clean up rubocop offences.

--- spec/1_1_convert_hex_to_base64_spec.rb
+++ spec/1_1_convert_hex_to_base64_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe '1-1' do
   it 'converts hex to base64' do
     hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
