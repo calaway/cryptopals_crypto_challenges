fb10fb31db77ee04540337268c6763b74bdfd978
Calaway <calaway@users.noreply.github.com>
2017-09-04 18:28:32 -0600

    Build out methods & script for 1.3. Still need to add character frequency analysis.

--- bin/1_3_single_byte_xor_cipher.rb
+++ bin/1_3_single_byte_xor_cipher.rb
@@ -1,17 +1,6 @@
-Dir['./bin/**/*.rb'].each { |file| require file }
-
-def letter_to_bite
-  { 'a' => '01100001', 'b' => '01100010', 'c' => '01100011', 'd' => '01100100', 'e' => '01100101',
-    'f' => '01100110', 'g' => '01100111', 'h' => '01101000', 'i' => '01101001', 'j' => '01101010',
-    'k' => '01101011', 'l' => '01101100', 'm' => '01101101', 'n' => '01101110', 'o' => '01101111',
-    'p' => '01110000', 'q' => '01110001', 'r' => '01110010', 's' => '01110011', 't' => '01110100',
-    'u' => '01110101', 'v' => '01110110', 'w' => '01110111', 'x' => '01111000', 'y' => '01111001',
-    'z' => '01111010', 'A' => '01000001', 'B' => '01000010', 'C' => '01000011', 'D' => '01000100',
-    'E' => '01000101', 'F' => '01000110', 'G' => '01000111', 'H' => '01001000', 'I' => '01001001',
-    'J' => '01001010', 'K' => '01001011', 'L' => '01001100', 'M' => '01001101', 'N' => '01001110',
-    'O' => '01001111', 'P' => '01010000', 'Q' => '01010001', 'R' => '01010010', 'S' => '01010011',
-    'T' => '01010100', 'U' => '01010101', 'V' => '01010110', 'W' => '01010111', 'X' => '01011000',
-    'Y' => '01011001', 'Z' => '01011010' }
+# require 'pry'; binding.pry
+Dir['./bin/**/*.rb'].each do |file|
+  require file unless file.split('/').last == __FILE__.split('/').last
 end
 
 def bite_to_letter
@@ -36,10 +25,13 @@ def bite_to_letter
     '01111010' => 'z', '01111011' => '{', '01111100' => '|', '01111101' => '}', '01111110' => '~' }
 end
 
+def letter_to_bite
+  @letter_to_bite ||= bite_to_letter.invert
+end
+
 def bites_to_letters(bites)
   plaintext = ''
   until bites.length < 8
-    # require 'pry'; binding.pry
     letter = bite_to_letter[bites[0..7]]
     letter.nil? ? break : plaintext << letter
     bites = bites[8..-1]
@@ -50,11 +42,17 @@ end
 ciphertext_hex = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 ciphertext_binary = hex_to_binary(ciphertext_hex)
 
-all_plaintexts = letter_to_bite.values.map do |bite|
+all_plaintexts = letter_to_bite.map do |letter, bite|
   repetitions = (ciphertext_binary.length / 8.0).ceil
   pad = bite * repetitions
   plaintext_bites = xor(ciphertext_binary, pad)
-  bites_to_letters(plaintext_bites)
+  [letter, bites_to_letters(plaintext_bites)]
+end.to_h
+
+max_length = all_plaintexts.map { |plaintext_pair| plaintext_pair[1].length }.max
+
+all_plaintexts = all_plaintexts.select do |_, plaintext|
+  plaintext.length == max_length
 end
 
-p all_plaintexts
+all_plaintexts.each { |plaintext| p plaintext }
