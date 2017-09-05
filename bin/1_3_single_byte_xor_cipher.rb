518ca860dc9517039c6ebd1de5a611fe48ba0fb4
Calaway <calaway@users.noreply.github.com>
2017-09-04 22:59:51 -0600

    Complete 1.3 and reorganize directory structure.

--- bin/1_3_single_byte_xor_cipher.rb
+++ bin/1_3_single_byte_xor_cipher.rb
@@ -1,43 +1,4 @@
-# require 'pry'; binding.pry
-Dir['./bin/**/*.rb'].each do |file|
-  require file unless file.split('/').last == __FILE__.split('/').last
-end
-
-def bite_to_letter
-  { '00100000' => ' ', '00100001' => '!', '00100010' => '"', '00100011' => '#', '00100100' => '$',
-    '00100101' => '%', '00100110' => '&', '00100111' => '\'', '00101000' => '(', '00101001' => ')',
-    '00101010' => '*', '00101011' => '+', '00101100' => ',', '00101101' => '-', '00101110' => '.',
-    '00101111' => '/', '00110000' => '0', '00110001' => '1', '00110010' => '2', '00110011' => '3',
-    '00110100' => '4', '00110101' => '5', '00110110' => '6', '00110111' => '7', '00111000' => '8',
-    '00111001' => '9', '00111010' => ':', '00111011' => ';', '00111100' => '<', '00111101' => '=',
-    '00111110' => '>', '00111111' => '?', '01000000' => '@', '01000001' => 'A', '01000010' => 'B',
-    '01000011' => 'C', '01000100' => 'D', '01000101' => 'E', '01000110' => 'F', '01000111' => 'G',
-    '01001000' => 'H', '01001001' => 'I', '01001010' => 'J', '01001011' => 'K', '01001100' => 'L',
-    '01001101' => 'M', '01001110' => 'N', '01001111' => 'O', '01010000' => 'P', '01010001' => 'Q',
-    '01010010' => 'R', '01010011' => 'S', '01010100' => 'T', '01010101' => 'U', '01010110' => 'V',
-    '01010111' => 'W', '01011000' => 'X', '01011001' => 'Y', '01011010' => 'Z', '01011011' => '[',
-    '01011100' => '\\', '01011101' => ']', '01011110' => '^', '01011111' => '_', '01100000' => '`',
-    '01100001' => 'a', '01100010' => 'b', '01100011' => 'c', '01100100' => 'd', '01100101' => 'e',
-    '01100110' => 'f', '01100111' => 'g', '01101000' => 'h', '01101001' => 'i', '01101010' => 'j',
-    '01101011' => 'k', '01101100' => 'l', '01101101' => 'm', '01101110' => 'n', '01101111' => 'o',
-    '01110000' => 'p', '01110001' => 'q', '01110010' => 'r', '01110011' => 's', '01110100' => 't',
-    '01110101' => 'u', '01110110' => 'v', '01110111' => 'w', '01111000' => 'x', '01111001' => 'y',
-    '01111010' => 'z', '01111011' => '{', '01111100' => '|', '01111101' => '}', '01111110' => '~' }
-end
-
-def letter_to_bite
-  @letter_to_bite ||= bite_to_letter.invert
-end
-
-def bites_to_letters(bites)
-  plaintext = ''
-  until bites.length < 8
-    letter = bite_to_letter[bites[0..7]]
-    letter.nil? ? break : plaintext << letter
-    bites = bites[8..-1]
-  end
-  plaintext
-end
+Dir['./support/**/*.rb'].each { |file| require file }
 
 ciphertext_hex = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 ciphertext_binary = hex_to_binary(ciphertext_hex)
@@ -50,9 +11,17 @@ all_plaintexts = letter_to_bite.map do |letter, bite|
 end.to_h
 
 max_length = all_plaintexts.map { |plaintext_pair| plaintext_pair[1].length }.max
-
 all_plaintexts = all_plaintexts.select do |_, plaintext|
   plaintext.length == max_length
 end
 
-all_plaintexts.each { |plaintext| p plaintext }
+scored_plaintexts = all_plaintexts.map { |plaintext| (plaintext << english_letter_frequency_score(plaintext.last)) }
+
+sorted_plaintexts = scored_plaintexts.sort_by { |scored_plaintext| scored_plaintext[2] }
+
+# sorted_plaintexts = all_plaintexts.sort_by do |plaintext|
+#   english_letter_frequency_score(letter_frequencies(plaintext.last))
+# end.to_h.values
+
+# require 'pry'; binding.pry
+sorted_plaintexts.each { |sorted_plaintext| p sorted_plaintext }
